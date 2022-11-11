2c9c766d4e07510ef0462a9467660d32a57809e0
Halkawt <halkawt.abdulilah@gmail.com>
2022-11-11 09:11:37 +0300

    added code + matrix addition (for now)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
-public class Main {
+import Controller.CalculatorController;
+import Model.CalculatorModel;
+import View.CalculatorView;
+
+public class Main{
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        CalculatorModel calculatorModel = new CalculatorModel(3, 3);
+        CalculatorView calculatorView = new CalculatorView(3, 3);
+        CalculatorController calculatorController = new CalculatorController(calculatorView, calculatorModel);
+        calculatorView.setVisible(true);
     }
 }
