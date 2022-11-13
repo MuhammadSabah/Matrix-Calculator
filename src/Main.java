a3ee198d67818faac0bbd48878593d3141adb3c2
Halkawt <halkawt.abdulilah@gmail.com>
2022-11-13 16:17:26 +0300

    multiplication fix

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
-
-import View.SetupView;
+import Controller.CalculatorController;
+import Model.CalculatorModel;
+import View.CalculatorView;
 
 public class Main {
     public static void main(String[] args) {
-        SetupView setupView = new SetupView();
-        setupView.setVisible(true);
+        CalculatorModel calculatorModel = new CalculatorModel(3, 3);
+        CalculatorView calculatorView = new CalculatorView(3, 3);
+        CalculatorController calculatorController = new CalculatorController(calculatorView, calculatorModel);
+        calculatorView.setVisible(true);
     }    
 }
